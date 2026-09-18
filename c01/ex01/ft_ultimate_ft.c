a4a5a04c3e65ab8c553b50a7ef11b97249e3fa5f
alamtafe <adekmadek24@gmail.com>
2026-09-18 13:22:29 +0100

    COmplete Ex01 in C01

---
+++ c01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,19 @@
+//#include <stdio.h>
+void ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+//int main()
+//{
+//	int x;
+//	int *x1 = &x;
+//	int **x2 = &x1;
+//	int ***x3 = &x2;
+//	int ****x4 = &x3;
+//	int *****x5 = &x4;
+//	int ******x6 = &x5;
+//	int *******x7 = &x6;
+//	int ********x8 = &x7;
+//	ft_ultimate_ft(&x8);
+//	printf("%d\n", x);
+//}
