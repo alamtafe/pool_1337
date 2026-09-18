fc0768524b197d87d84dbe223852b8ffed32c4be
alamtafe <adekmadek24@gmail.com>
2026-09-18 13:43:58 +0100

    Complete Ex02 in C01

---
+++ c01/ex02/ft_swap.c
@@ -0,0 +1,15 @@
+//#include <stdio.h>
+void ft_swap(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b =  temp;
+}
+//int main()
+//{
+//	int a = 1;
+//	int b = 2;
+//	ft_swap(&a,&b);
+//	printf("%d\n%d\n",a,b);
+//}
